6804ce982cd55457408a915f71cb572d7a273eab
shivanshu66 <162125439+shivanshusawan66@users.noreply.github.com>
2024-11-12 11:24:48 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,10 @@
+Sales Dashboard 📊
+This is a web-based sales dashboard built with Streamlit and Plotly, designed to display key performance indicators (KPIs) and visualizations from a supermarket sales dataset. The dashboard allows users to filter data by city, customer type, and gender, and includes options to download the dataset in Excel format.
+
+Features
+Interactive Filtering: Filter sales data by city, customer type, and gender in the sidebar.
+Key Performance Indicators: Displays total sales, average rating, and average sales per transaction.
+Sales by Product Line: Horizontal bar chart to show sales breakdown by product line.
+Hourly Sales: Bar chart showing sales trends across different hours.
+Dataset Download: Download the dataset directly from the dashboard.
+Custom Styling: Minimalist dark-themed styling for an enhanced user experience.
---
+++ app.py
@@ -0,0 +1,164 @@
+import pandas as pd  # pip install pandas openpyxl
+import plotly.express as px  # pip install plotly-express
+import streamlit as st  # pip install streamlit
+
+# emojis: https://www.webfx.com/tools/emoji-cheat-sheet/
+st.set_page_config(page_title="Sales Dashboard", page_icon=":bar_chart:", layout="wide")
+
+
+# ---- READ EXCEL ----
+@st.cache_data
+def get_data_from_excel():
+    df = pd.read_excel(
+        io="supermarkt_sales.xlsx",
+        engine="openpyxl",
+        sheet_name="Sales",
+        skiprows=3,
+        usecols="B:R",
+        nrows=1000,
+    )
+    # Add 'hour' column to dataframe
+    df["hour"] = pd.to_datetime(df["Time"], format="%H:%M:%S").dt.hour
+    return df
+
+df = get_data_from_excel()
+
+# ---- SIDEBAR ----
+st.sidebar.header("Please Filter Here:")
+city = st.sidebar.multiselect(
+    "Select the City:",
+    options=df["City"].unique(),
+    default=df["City"].unique()
+)
+
+customer_type = st.sidebar.multiselect(
+    "Select the Customer Type:",
+    options=df["Customer_type"].unique(),
+    default=df["Customer_type"].unique(),
+)
+
+gender = st.sidebar.multiselect(
+    "Select the Gender:",
+    options=df["Gender"].unique(),
+    default=df["Gender"].unique()
+)
+
+df_selection = df.query(
+    "City == @city & Customer_type ==@customer_type & Gender == @gender"
+)
+
+# Check if the dataframe is empty:
+if df_selection.empty:
+    st.warning("No data available based on the current filter settings!")
+    st.stop() # This will halt the app from further execution.
+
+# ---- MAINPAGE ----
+st.title(":bar_chart: Sales Dashboard")
+
+st.markdown("##")
+
+# TOP KPI's
+total_sales = int(df_selection["Total"].sum())
+average_rating = round(df_selection["Rating"].mean(), 1)
+star_rating = ":star:" * int(round(average_rating, 0))
+average_sale_by_transaction = round(df_selection["Total"].mean(), 2)
+
+left_column, middle_column, right_column = st.columns(3)
+with left_column:
+    st.subheader("Total Sales:")
+    st.subheader(f"US $ {total_sales:,}")
+with middle_column:
+    st.subheader("Average Rating:")
+    st.subheader(f"{average_rating} {star_rating}")
+with right_column:
+    st.subheader("Average Sales Per Transaction:")
+    st.subheader(f"US $ {average_sale_by_transaction}")
+
+st.markdown("""---""")
+
+# SALES BY PRODUCT LINE [BAR CHART]
+sales_by_product_line = df_selection.groupby(by=["Product line"])[["Total"]].sum().sort_values(by="Total")
+fig_product_sales = px.bar(
+    sales_by_product_line,
+    x="Total",
+    y=sales_by_product_line.index,
+    orientation="h",
+    title="<b>Sales by Product Line</b>",
+    color_discrete_sequence=["#0083B8"] * len(sales_by_product_line),
+    template="plotly_white",
+)
+fig_product_sales.update_layout(
+    plot_bgcolor="rgba(0,0,0,0)",
+    xaxis=(dict(showgrid=False))
+)
+fig_product_sales.update_xaxes(tickvals=[i * 10000 for i in range(1, int(sales_by_product_line["Total"].max() // 10000) + 2)])
+
+# SALES BY HOUR [BAR CHART]
+sales_by_hour = df_selection.groupby(by=["hour"])[["Total"]].sum()
+fig_hourly_sales = px.bar(
+    sales_by_hour,
+    x=sales_by_hour.index,
+    y="Total",
+    title="<b>Sales by hour</b>",
+    color_discrete_sequence=["#0083B8"] * len(sales_by_hour),
+    template="plotly_white",
+)
+fig_hourly_sales.update_layout(
+    xaxis=dict(tickmode="linear"),
+    plot_bgcolor="rgba(0,0,0,0)",
+    yaxis=(dict(showgrid=False)),
+)
+
+
+left_column, right_column = st.columns(2)
+left_column.plotly_chart(fig_hourly_sales, use_container_width=True)
+right_column.plotly_chart(fig_product_sales, use_container_width=True)
+
+#-----Dataset Download--------
+# Section header for downloading the dataset
+st.markdown("####  Download Dataset")
+st.markdown("Click the button below to download the `Supermarket Sales dataset` in Excel format:")
+# Path to the existing .xlsx file
+file_path = "supermarkt_sales.xlsx"
+
+# Function to load and provide download
+def get_file(file_path):
+    with open(file_path, "rb") as f:
+        return f.read()
+
+# Download button for the existing .xlsx file
+file_data = get_file(file_path)
+
+st.download_button(
+    label="Download Supermarket Sales Data",
+    data=file_data,
+    file_name="supermarkt_sales.xlsx",
+    mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+)
+
+
+# ---- HIDE STREAMLIT STYLE ----
+hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+
+            .footer {
+            position: fixed;
+            left: 0;
+            bottom: 0;
+            width: 100%;
+            background-color: #00172B;
+            color: white;
+            text-align: center;
+            padding: 10px;
+            }
+            </style>
+            <div class="footer">
+                <p>© 2024 Shivanshu Sawan | UIET Chandigarh, Panjab University</p>
+                <p>Contact: <a href="mailto:shivanshusawan66@gmail.com">shivanshusawan66@gmail.com</a> | 
+                GitHub: <a href="https://github.com/shivanshusawan66/KPI-Dashboard">Project Repository</a></p>
+            </div>
+            """
+st.markdown(hide_st_style, unsafe_allow_html=True)
